8d4483d7318f2bdaa6751383b95e3634f400697f
희주 <149589749+heeju97@users.noreply.github.com>
2024-02-01 21:32:16 +0900

    React

---
+++ data.js
@@ -0,0 +1,54 @@
+// 길고 복잡한 변수 보관
+// 함수, 컴포넌트, 변수 등 가능
+
+
+
+
+let data = [
+// {
+//     id:0,
+//     title: 'product1',
+//     content: '어쩌구1',
+//     price: 15900
+// },
+// {
+//     id:1,
+//     title: 'product2',
+//     content: '어쩌구2',
+//     price: 25900
+// },
+// {
+//     id:2,
+//     title: 'product3',
+//     content: '어쩌구3',
+//     price: 26300
+// }
+
+{
+    id:0,
+    title: 'product1',
+    content: '내용1',
+    price: 15900
+},
+{
+    id:1,
+    title: 'product2',
+    content: '내용2',
+    price:12900235
+},
+{
+    id:2,
+    title: 'product3',
+    content: '내용3',
+    price:15515235900
+}
+
+]
+
+const temp1 =[1, 2, 3];
+const temp2 = [4, 5, 6];
+
+const temp3 = [1,2,3,...temp2];
+console.log(temp3);
+
+export default data ;
